75ace74e7b1717d819c8ab6a24ac636540c4b888
miu <38824651+miu200521358@users.noreply.github.com>
2025-11-22 19:52:51 +0900

    Stop tracking Terraform lock file

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,9 @@
+resource "google_project_service" "cloud_run_api" {
+  project            = var.project_id
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_storage_bucket" "db_bucket" {
   name                        = var.gcs_bucket_name
   location                    = var.region
@@ -79,7 +85,10 @@ resource "google_cloud_run_v2_service" "llm_webui" {
     percent = 100
   }
 
-  depends_on = [google_storage_bucket.db_bucket]
+  depends_on = [
+    google_project_service.cloud_run_api,
+    google_storage_bucket.db_bucket,
+  ]
 }
 
 resource "google_cloud_run_v2_service_iam_member" "invoker" {
